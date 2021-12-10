06322a2ae59ec08b1e083800cabb54948ff4a43b
Alexamakans <fougner.alexander@gmail.com>
2021-12-10 09:30:16 +0100

    Changed access perms for files

--- make.sh
+++ make.sh
@@ -17,4 +17,4 @@ npm init -y
 echo "const fs = require('fs');\n\nconst testPath = '../input_test.txt';\nconst realPath = '../input.txt';\n\nfunction exec(path) {\n\tconst data = fs.readFileSync(path, { encoding: 'utf-8' });\n\tconst lines = data.split(/\\\\r?\\\\n/);\n\tlet result;\n\treturn result;\n}\n\nconst testResult = exec(testPath);\nif (testResult != 7) {\n\tthrow \`Test failed: \${testResult}\`;\n}\n\nconsole.log(exec(realPath));" > index.js
 
 
-chmod +777 -R ../../day$day
+chmod 775 -R ../../day$day
