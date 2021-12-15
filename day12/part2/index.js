337ee40debc5651f8d028e57058d3c800e093acc
Alexamakans <fougner.alexander@gmail.com>
2021-12-15 17:52:32 +0100

    day12

---
+++ day12/part2/index.js
@@ -0,0 +1,63 @@
+const { constants } = require('buffer');
+const fs = require('fs');
+
+const testPath = '../input_test.txt';
+const realPath = '../input.txt';
+
+function zip(a, b) {
+	return [[a[0], b[0]], [a[1], b[1]]];
+}
+
+function exec(path) {
+	const data = fs.readFileSync(path, { encoding: 'utf-8' });
+	const pairs = data.split(/\r?\n/).filter(s => s.length > 0).map(s => s.split('-'));
+
+	const conns = {};
+	for (const pair of pairs) {
+		const rev = [pair[1], pair[0]];
+		for (const [p1, p2] of zip(pair, rev)) {
+			if (p2 != 'start') {
+				if (!conns[p1]) {
+					conns[p1] = [];
+				}
+				conns[p1].push(p2);
+			}
+		}
+	}
+	delete conns['end'];
+
+	function rec1(data, path=['start']) {
+		let res = 0;
+		for (const p of data[path[path.length - 1]]) {
+			if (p.toUpperCase() === p || !path.includes(p)) {
+				if (p == 'end') {
+					res++;
+				} else {
+					res += rec1(data, [...path, p]);
+				}
+			}
+		}
+		return res;
+	}
+	function rec2(data, path=['start']) {
+		let res = 0;
+		for (const p of data[path[path.length - 1]]) {
+			if (p == 'end') {
+				res++;
+			} else if (p.toLowerCase() === p && path.includes(p)) {
+				res += rec1(data, [...path, p]);
+			} else {
+				res += rec2(data, [...path, p]);
+			}
+		}
+		return res;
+	}
+	return rec2(conns);
+}
+
+const testResult = exec(testPath);
+if (testResult != 36) {
+	throw `Test failed: ${testResult}`;
+}
+
+console.log(exec(realPath));
