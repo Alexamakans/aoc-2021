0eee49830744047bc98288ee6b65d6243a65a68e
Alexamakans <fougner.alexander@gmail.com>
2021-12-17 19:52:14 +0100

    day17

---
+++ day17/part1/index.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+
+const testPath = '../input_test.txt';
+const realPath = '../input.txt';
+
+function drag(x, amt=-1) {
+	return Math.max(x + amt, 0);
+}
+
+function gravity(y, amt=-1) {
+	return y + amt;
+}
+
+function inside(x, y, left, right, top, bottom) {
+	return x >= left && x <= right && y >= bottom && y <= top;
+}
+
+function exec(path) {
+	const matches = fs.readFileSync(path, { encoding: 'utf-8' }).matchAll(/(-?\d+)\.\.(-?\d+)/g);
+	let [ , left, right ] = matches.next().value;
+	left = parseInt(left);
+	right = parseInt(right);
+	let [ , bottom, top ] = matches.next().value;
+	bottom = parseInt(bottom);
+	top = parseInt(top);
+
+	let overallHighY = -1e16;
+	for (let startVelY = 0; startVelY < 1000; startVelY++) {
+		for (let startVelX = 0; startVelX < 1000; startVelX++) {
+			let x = 0;
+			let y = 0;
+			let dx = startVelX;
+			let dy = startVelY;
+			let curHighY = y;
+			while (dy >= 0 || y >= bottom) {
+				x += dx;
+				y += dy;
+				if (y > curHighY) {
+					curHighY = y;
+				}
+
+				dx = drag(dx);
+				dy = gravity(dy);
+
+				if (inside(x, y, left, right, top, bottom)) {
+					if (curHighY > overallHighY) {
+						overallHighY = curHighY;
+					}
+				}
+			}
+		}
+	}
+
+	return overallHighY;
+}
+
+const testResult = exec(testPath);
+if (testResult != 45) {
+	throw `Test failed: ${testResult}`;
+}
+
+console.log(exec(realPath));
